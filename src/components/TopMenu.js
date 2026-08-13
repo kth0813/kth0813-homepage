09b54936d5fcb93daa18b9daadd9322cb00faefd
kth0813 <ksskth0813@naver.com>
2026-08-14 00:14:15 +0900

    ㅓㅓ

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState, useCallback } from "react";
 import { dbService } from "../services/DbService";
-import { showToast } from "../utils/Alert";
 
 function TopMenu({ onMenuToggle }) {
   const navigate = useNavigate();
--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -160,7 +160,7 @@ function BoardDetail() {
         <ReactMarkdown
           children={post.contents}
           components={{
-            code({ node, inline, className, children, ...props }) {
+            code({ inline, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || "");
               return !inline && match ? (
                 <SyntaxHighlighter children={String(children).replace(/\n$/, "")} style={atomDark} language={match[1]} PreTag="div" {...props} />
